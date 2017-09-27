fcbd71036387455ff0c40aada384519994a92fc6
Oliver Zheng <gitcommits@oliverzheng.com>
2017-09-27 15:53:05 -0500

    [server] Fix mysql connection timeouts
    This happened when the connection wasn't used for a few hours (the default mysql
    connection timeout), and the next time anyone uses it, it crashes the app. This
    needs to be fixed in 2 ways: automatically regain the connection, and also
    a connection failure shouldn't crash the app. This commit fixes the first by
    using a connection pool which automatically reconnects.

--- src/storage/mysql.js
+++ src/storage/mysql.js
@@ -9,13 +9,22 @@ export async function connectToMySQL(
   dbURL: string,
 ): Promise<Connection> {
   return await new Promise((resolve, reject) => {
-    const connection = mysql.createConnection(dbURL);
-    connection.connect((err) => {
+    const pool = mysql.createPool(`${dbURL}?acquireTimeout=null`);
+    // We don't need to add additional logic to check to see the connection is
+    // still alive (didn't timeout) when we fetch it later. The pool makes sure
+    // each connection is still usable when we grab it from the pool. There's an
+    // optimization to be done here though, where we grab a connection for each
+    // request and assign it to the duration fo the request, so we don't ping
+    // for every request. TODO
+
+    // Verify we can create at least 1 connection.
+    pool.getConnection((err, conn) => {
       if (err) {
         reject('Cannot connect to db: ' + err);
         return;
       }
-      resolve(connection);
+      conn.release();
+      resolve(pool);
     });
   });
 }
