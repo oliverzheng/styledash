2b95c0410f8426b787e11652588ffefea85dacfd
Oliver Zheng <gitcommits@oliverzheng.com>
2017-10-02 20:38:26 -0500

    [util] Make printError print the error call stack

--- src/consoleUtil.js
+++ src/consoleUtil.js
@@ -1,5 +1,6 @@
 /** @flow */
 
+import invariant from 'invariant';
 // Emits webpack warning - see https://github.com/Marak/colors.js/issues/137
 import colors from 'colors/safe';
 
@@ -11,6 +12,15 @@ export function printActionResult(result: string): void {
   console.log('    ' + result);
 }
 
-export function printError(error: string): void {
-  console.log(colors.red(error));
+export function printError(error: Object | string): void {
+  let msg;
+  if (typeof error === 'string') {
+    msg = error;
+  } else if (error instanceof Error) {
+    msg = error.stack;
+  } else {
+    invariant(false, 'Found unusable error');
+  }
+
+  console.log(colors.red(msg));
 }
