432b545e5fbd1ae2993707feccdef602258b4541
Oliver Zheng <gitcommits@oliverzheng.com>
2017-10-09 20:53:05 -0500

    [server] Rename schema's component indices to be more clear

--- src/schemas/styledash-database/component.sql
+++ src/schemas/styledash-database/component.sql
@@ -9,7 +9,7 @@ CREATE TABLE `component` (
   `override_react_doc` longtext,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
-  UNIQUE KEY `repository_path` (`name`,`repository_id`,`filepath`,`is_named_export`),
-  KEY `repository_id` (`repository_id`,`is_named_export`),
-  KEY `repository_id_2` (`repository_id`,`filepath`)
+  UNIQUE KEY `component_identity` (`name`,`repository_id`,`filepath`,`is_named_export`),
+  KEY `repository_components` (`repository_id`,`is_named_export`),
+  KEY `all_components_at_path` (`repository_id`,`filepath`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
